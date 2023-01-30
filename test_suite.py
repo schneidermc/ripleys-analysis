7ee460faaf41dda14150c16e4b925f7d9e84de20
Magdalena Schneider <schneiderm2@hhmi.org>
2023-01-30 15:50:48 -0500

    Add tests for cross Ripley's analysis

--- test_suite.py
+++ test_suite.py
@@ -11,6 +11,8 @@ import dataModule as dm
 import maskModule as mm
 import ripleysModule as rm
 
+np.random.seed(10) # initialize random seed
+
 class TestRandomDistribution(unittest.TestCase):
     
     def test_randomCase(self):
@@ -37,6 +39,45 @@ class TestRandomDistribution(unittest.TestCase):
         nPoints = 10000
         pointsA, *__ = cellMask.randomPoints(nPoints)
         pointsB, *__ = cellMask.randomPoints(nPoints)
+        radii = np.arange(10, 50, 2)
+        cellMask.plotPoints(pointsA, title='Test data 1')
+        cellMask.plotPoints(pointsB, title='Test data 2')
+        
+        results = rm.CrossRipleysAnalysis(pointsA, pointsB, radii, cellMask, nControls=200)
+        results.plot(ci=0.95, normalized=True, showControls=True, title='Cross Ripleys (normalized)')
+        results.plot(ci=0.95, normalized=False, showControls=True, title='Cross Ripleys')
+        
+    def test_cross_noInteraction_clusteredA_randomB(self):
+        path = "./data/Cell3"
+        filename = 'MutuDC_6h_stimuli'
+        pixelsize = 130
+        cellMask = mm.loadMask(path, f'{filename}_mask.npy', pixelsize)
+        
+        nPoints = 10000
+        pointsA, *__ = cellMask.randomPoints(nPoints)
+        ind = int(nPoints/3)
+        pointsA = np.vstack([pointsA[0:ind,:], pointsA[0:ind,:], pointsA[0:ind,:]])
+        
+        pointsB, *__ = cellMask.randomPoints(nPoints)
+        radii = np.arange(10, 50, 2)
+        cellMask.plotPoints(pointsA, title='Test data 1')
+        cellMask.plotPoints(pointsB, title='Test data 2')
+        
+        results = rm.CrossRipleysAnalysis(pointsA, pointsB, radii, cellMask, nControls=200)
+        results.plot(ci=0.95, normalized=True, showControls=True, title='Cross Ripleys (normalized)')
+        results.plot(ci=0.95, normalized=False, showControls=True, title='Cross Ripleys')
+        
+    def test_cross_noInteraction_randomA_clusteredB(self):
+        path = "./data/Cell3"
+        filename = 'MutuDC_6h_stimuli'
+        pixelsize = 130
+        cellMask = mm.loadMask(path, f'{filename}_mask.npy', pixelsize)
+        
+        nPoints = 10000
+        pointsA, *__ = cellMask.randomPoints(nPoints)
+        pointsB, *__ = cellMask.randomPoints(nPoints)
+        ind = int(nPoints/3)
+        pointsB = np.vstack([pointsB[0:ind,:], pointsB[0:ind,:], pointsB[0:ind,:]])
         radii = np.arange(10, 50, 2)
         cellMask.plotPoints(pointsA, title='Test data 1')
         cellMask.plotPoints(pointsB, title='Test data 2')
