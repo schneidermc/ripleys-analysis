f646424cf2b88f5834786b27958c7a37257ea9a6
Magdalena Schneider <schneiderm2@hhmi.org>
2023-01-30 15:43:49 -0500

    Add breakline in console output

--- dataModule.py
+++ dataModule.py
@@ -58,6 +58,7 @@ class LocalizationData:
         for k in tqdm(range(self.nReceptors)):
             tree = KDTree(self.data[k])
             forest.append(tree)
+        print('\n')
         return forest
             
     def plot(self, receptor='all', title=None):
